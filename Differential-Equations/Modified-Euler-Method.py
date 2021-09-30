ed8287f10f30adc254f1dc190460a7a9a6bb7296
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2021-09-30 18:33:14 +0300

    Added Adams-Bashforth-Method.py, Modified-Euler-Method.py, Predictor-Corrector.py, Shot-Euler-Method.py, renamed Maximum-Range-Computation.py to Maximum-Range-Runge-Kutta-Fourth-Order.py, modified Euler-Method.py

--- Differential-Equations/Modified-Euler-Method.py
+++ Differential-Equations/Modified-Euler-Method.py
@@ -8,14 +8,14 @@ def f(t, y):
     return 0.5*y
 
 # a,b: edges of the interval, w0:ititial value, h:step
-def Euler(a, b, w0, h):
+def ModifiedEuler(a, b, w0, h):
     n = ceil((b-a)/h)
 
     w, t = np.zeros(n+1), np.zeros(n+1)
     w[0], t[0] = w0, a
 
     for i in range(n):
-        w[i+1] = w[i] + h*f(t[i], w[i])
+        w[i+1] = w[i] + h*f(t[i] + h/2, w[i] + h/2*f(t[i], w[i]))
         t[i+1] = t[i] + h
 
     return w, t
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     a, b, w0, h = 0, 2, 1, 0.2
 
-    print(Euler(a, b, w0, h)[0])
+    print(ModifiedEuler(a, b, w0, h)[0])
 
     # Visualization
     x = np.linspace(0, 2, 100)
@@ -35,8 +35,9 @@ if __name__ == '__main__':
         y[j] = exp(0.5*x[j])
 
     plt.plot(x, y, color="black", label="Solution")
-    plt.plot(Euler(a, b, w0, h)[1], Euler(a, b, w0, h)[0], label="Euler")
+    plt.plot(ModifiedEuler(a, b, w0, h)[1], ModifiedEuler(a, b, w0, h)[0], label="ModEuler")
     plt.xlabel("t")
     plt.ylabel("y")
     plt.legend()
     plt.show()
+
